7d9787e88e04170e9f554243ccd7ecc19540c7c9
Ariel Smith <arieljeansmith@gmail.com>
2022-05-13 19:28:58 -0700

    Item model created

--- inventoryTracking/models.py
+++ inventoryTracking/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    quantity = models.IntegerField
+    is_deleted = models.BooleanField(default=False)
+    deletion_comment = models.TextField(max_length=2000)
+
+    def __str__(self):
+        return self.name
